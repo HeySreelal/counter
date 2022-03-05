48c6f86f162ae9278cf0174c455bcff614a0932d
Sreelal TS <heysreelal@gmail.com>
2022-03-05 11:35:04 +0530

    Added keyboard events & minor improvements ��

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
 import 'package:counter/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
@@ -11,52 +13,76 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int count = 0;
+
+  void reset() => setState(() => count = 0);
+  void increment() => setState(() => count++);
+  void decrement() => setState(() {
+        if (count > 0) {
+          count--;
+        } else {
+          showMsg("Excuse me, let's count to positive.");
+        }
+      });
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        setState(() => count++);
-      },
-      onDoubleTap: () {
-        if(count > 0) {
-          setState(() => count--);
-        }
-      },
-      child: Scaffold(
-        body: Stack(
-          alignment: Alignment.center,
-          fit: StackFit.expand,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                gradient: TaylorsTheme.randomGradient(),
+      onTap: () => increment(),
+      onDoubleTap: () => decrement(),
+      child: RawKeyboardListener(
+        focusNode: FocusNode(),
+        autofocus: true,
+        onKey: (value) {
+          // If SPACE or UP Arrow Key is pressed, increment the counter.
+          if (value.isKeyPressed(LogicalKeyboardKey.space) ||
+              value.isKeyPressed(LogicalKeyboardKey.arrowUp)) increment();
+
+          // If DOWN Arrow Key is pressed, decrement the counter.
+          if (value.isKeyPressed(LogicalKeyboardKey.arrowDown)) decrement();
+
+          // if Delete or Backspace Key is pressed, reset the counter.
+          if (value.isKeyPressed(LogicalKeyboardKey.delete) ||
+              value.isKeyPressed(LogicalKeyboardKey.backspace)) reset();
+        },
+        child: Scaffold(
+          body: Stack(
+            alignment: Alignment.center,
+            fit: StackFit.expand,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  gradient: TaylorsTheme.randomGradient(),
+                ),
               ),
-            ),
-            Positioned(
-              child: Center(
-                child: Text(
-                  '$count',
-                  style: GoogleFonts.lobster(
-                    textStyle: const TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 100,
+              Positioned(
+                child: Center(
+                  child: Text(
+                    '$count',
+                    style: GoogleFonts.lobster(
+                      textStyle: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 100,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              count = 0;
-            });
-          },
-          child: const Icon(Icons.restore),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => reset(),
+            child: const Icon(Icons.restore),
+          ),
         ),
       ),
     );
   }
+}
+
+void showMsg(String msg) {
+  Fluttertoast.showToast(
+    msg: msg,
+    timeInSecForIosWeb: 3,
+  );
 }
